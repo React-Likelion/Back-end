406dd451c1938fcaf80c0792c7b8cccf603a4100
hxeyexn <kck85075@kumoh.ac.kr>
2022-08-07 19:47:51 +0900

    Update Signup&Login(account activate), Create Cookie&Logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,30 +1,52 @@
 import traceback
 from .models import Members
 from .tokens import account_activation_token
+from .serializers import SignupSerializer, LoginSerializer
 
-from rest_framework import status, generics, views, viewsets
+from rest_framework import status, generics, views
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-
-from .serializers import SignupSerializer, LoginSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from django.utils.encoding import force_str
 from django.utils.http import urlsafe_base64_decode
 from django.http import HttpResponse
 
+
 class MembersListView(generics.ListAPIView):
+    permission_classes = [AllowAny,]
     queryset = Members.objects.all()
     serializer_class = SignupSerializer
 
 class SignupView(generics.CreateAPIView):
-    queryset = Members.objects.all()
+    permission_classes = [AllowAny,]
+    #queryset = Members.objects.all()
     serializer_class = SignupSerializer
     def post(self, request):
-      serializer = self.get_serializer(data=request.data)
-      if serializer.is_valid():
-          serializer.save()
-          return Response(serializer.data, status=status.HTTP_201_CREATED)
-      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "register successs",
+                    "token": {
+                        "access_token": access_token,
+                        "refresh_token": refresh_token,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+            
+            # jwt 토큰 => 쿠키에 저장
+            res.set_cookie("access_token", access_token, httponly=True)
+            res.set_cookie("refresh_token", refresh_token, httponly=True)
+            
+            return res
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserActivate(views.APIView):
     permission_classes = [AllowAny,]
@@ -49,10 +71,19 @@ class UserActivate(views.APIView):
 
 class LoginView(generics.GenericAPIView):
     serializer_class = LoginSerializer
+    permission_classes = [AllowAny,]
     
+    # Login
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         
         serializer.is_valid(raise_exception = True)
         token = serializer.validated_data
         return Response({"token":token}, status=status.HTTP_200_OK)
+   
+    # Logout
+    def delete(self, request):
+        response = Response({"message": "Logout success"}, status=status.HTTP_202_ACCEPTED)
+        response.delete_cookie("access_token")
+        response.delete_cookie("refresh_token")
+        return response
--- react/settings.py
+++ react/settings.py
@@ -75,9 +75,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 
-    #'DEFAULT_PERMISSION_CLASSES': [
-    #    'rest_framework.permissions.IsAuthenticated',
-    #],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
+    ],
 
 }
 

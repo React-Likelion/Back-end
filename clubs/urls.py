1357a18688a983287978464b5e80433931c8149f
Seo <daki403.1@gmail.com>
2022-08-07 15:28:38 +0900

    article function finished

--- clubs/urls.py
+++ clubs/urls.py
@@ -4,7 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.clubs_list),
-    path('<int:pk>/', views.clubs_detail),
+   
     path('by-newset/', views.clubs_new_list),
     path('by-member/', views.clubs_member_list),
+    
+    path('<int:pk>/', views.clubs_detail), 
+    path('<int:pk>/articles/', views.clubs_detail), 
+    #path('<int:pk>/articles/<int:pk_2>', views.clubs_detail),
+
+    #path('<int:pk>/galleries', views.clubs_detail), 
+    #path('<int:pk>/galleries/<int:pk_2>', views.clubs_detail), 
 ]
--- clubs/views.py
+++ clubs/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.db.models import Count
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import *
 from .serializer import *
 
 class ClubsViewSet(ModelViewSet):
+    lookup_field = 'pk'
+    lookup_url_kwarg = 'pk_2'
     queryset = Clubs.objects.all()
-    serializer_class = ClubsSerializer 
+    serializer_class = ClubsSerializer
+
+    @action(detail=True)
+    def articles(self, request, pk):
+        board_queryset = Clubboard.objects.filter(clud_id=pk)
+        serializer = self.get_serializer(board_queryset, many=True)
+        return Response(serializer.data)
+
 
 
 class ClubsNewViewSet(ModelViewSet):
@@ -19,6 +30,7 @@ class ClubsMemberViewSet(ModelViewSet):
                 .annotate(member_cnt=Count('member'))\
                 .order_by('-member_cnt')
     serializer_class = ClubsSerializer
+
                 
 
 clubs_list = ClubsViewSet.as_view({

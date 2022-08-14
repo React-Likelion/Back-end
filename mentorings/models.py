2bb09450eb241a3b753eb3085be0c3281067af5b
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-08-14 17:11:28 +0900

    Update models.py

--- mentorings/models.py
+++ mentorings/models.py
@@ -34,6 +34,14 @@ CATEGORY = [
 
 
 LOCATIIONS = [
+    ('서울특별시', '서울특별시'),
+    ('부산광역시', '부산광역시'),
+    ('대구광역시', '대구광역시'),
+    ('인천광역시', '인천광역시'),
+    ('광주광역시', '광주광역시'),
+    ('대전광역시', '대전광역시'),
+    ('울산광역시', '울산광역시'),
+    ('세종특별자치시', '세종특별자치시'), 
     ('경기도', '경기도'),
     ('강원도', '강원도'),
     ('충청북도', '충청북도'),
@@ -42,6 +50,7 @@ LOCATIIONS = [
     ('전라남도', '전라남도'),
     ('경상북도', '경상북도'),
     ('경상남도', '경상남도'),
+    ('제주특별자치도', '제주특별자치도')
 ]
 
 class mentorings(models.Model):
@@ -55,7 +64,7 @@ class mentorings(models.Model):
     title=models.TextField(null=True, blank=False);
     description=models.TextField(null=True, blank=False);
 
-    age_group=models.IntegerField(null=True, blank=False);
+    age_group=models.CharField(max_length=255, null=True, blank=False);
     limit=models.IntegerField(default=0,null=True, blank=False);
     tag=models.CharField(max_length=255, null=True, blank=False);
     tag2=models.CharField(max_length=255, null=True, blank=False);

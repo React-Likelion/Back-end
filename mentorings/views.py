9ecd39e33dcb2a9c95e3c17c36f6fdcfc9eff58f
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-08-14 19:38:12 +0900

    Update views.py

--- mentorings/views.py
+++ mentorings/views.py
@@ -56,7 +56,7 @@ class MentoringViewSet(viewsets.ModelViewSet):
         
     @action(detail=False)    
     def listbycnt(self, request, *args, **kwargs):
-        queryset = mentorings.objects.all().order_by('-member_cnt' , '-create_date')
+        queryset = mentorings.objects.all().order_by('-member_cnt')
 
         page = self.paginate_queryset(queryset)
         if page is not None:

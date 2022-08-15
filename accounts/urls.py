67b276a6cb816fc70dae6b451a51d3e277e4ec1b
hxeyexn <kck85075@kumoh.ac.kr>
2022-08-16 03:17:54 +0900

    Create UserUpdate, Withdraw

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from accounts.views import UserListView, SignupView, UserActivate, LoginView
+from accounts.views import UserListView, SignupView, UserActivate, LoginView, UserUpdateView
 
 #router = routers.DefaultRouter()
 #router.register(r'signup', SignupViewSet)
@@ -12,6 +12,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', UserListView.as_view(), name='user_list'),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('<int:pk>/update/', UserUpdateView.as_view(), name='update'),
     path('login/', LoginView.as_view(), name='login'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('activate/<str:uidb64>/<str:token>/', UserActivate.as_view(), name="activate"),

e4eed93efbeac0ca818c5f659be178e3a0b69af3
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-08-19 04:33:46 +0900

    Update urls.py

--- lectures/urls.py
+++ lectures/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import LecturesViewSet, LecturesEnrollViewSet, LecturesLikeViewSet
+from .views import LecturesViewSet, LecturesEnrollViewSet, LecturesLikeViewSet, MypageViewSet, MypageLecturesViewSet, MainPageViewSet
 from rest_framework.routers import DefaultRouter
 
 #라우팅 설정
@@ -26,9 +26,26 @@ lectures_like = LecturesLikeViewSet.as_view({
     'patch': 'update',
 })
 
+mypage_list = MypageViewSet.as_view({
+    'get': 'list',
+})
+
+mypagelectures_list = MypageLecturesViewSet.as_view({
+    'get': 'list',
+})
+
+main_list = MainPageViewSet.as_view({
+    'get': 'list',
+})
+
+
+
 urlpatterns =[
     path('', include(lectures_router.urls)),
     path('<int:pk>/enroll/',lectures_enroll),
     path('<int:pk>/like/',lectures_like),
+    path('mypage/', mypage_list),
+    path('mypagelectures/', mypagelectures_list),
+    path('main/', main_list),
 
 ] 

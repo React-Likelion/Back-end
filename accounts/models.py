4d937defd95d4b727a29d85efd02d50162853db0
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-08-18 22:10:03 +0900

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -109,7 +109,7 @@ class User(AbstractBaseUser):
     birth = models.DateField(auto_now_add=False)
     job = models.CharField(max_length=30, choices=JOB_CHOICES)
     date_joined = models.DateTimeField(auto_now_add=True) 
-    image = models.ImageField(blank=True, upload_to="accounts/", null=True, unique=True)
+    image = models.ImageField(blank=True, upload_to="accounts/", default = 'media/accounts/user.png')
 
     #point
     point = models.IntegerField(default=10000)

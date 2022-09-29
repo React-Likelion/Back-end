9a8d3f2b282d8f7c80f90d1e0f321e759844f10d
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-09-29 19:59:22 +0900

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -142,5 +142,5 @@ class UserDetailViewSet(viewsets.ModelViewSet):
 
 user_detail = UserDetailViewSet.as_view({
     'get': 'retrieve',
-    'patch' : 'partial_update'
+    'patch' : 'partial_update',
 })

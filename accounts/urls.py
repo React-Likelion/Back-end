b33e54181c5d6a6a99269471ba70ad049e88cb34
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-10-06 10:15:33 +0900

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,12 +3,13 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.routers import DefaultRouter
 from . import views
 
-from accounts.views import UserListView, SignupView, UserActivate, LoginView, UserUpdateView, PointViewSet, UserPointView, UserUpdateView
+from accounts.views import UserListView, SignupView, UserActivate, LoginView, UserUpdateView, PointViewSet, UserPointView, UserUpdateView, UserDetailViewSet
 
 #라우팅 설정
 router = DefaultRouter()
 router.register('', PointViewSet)
-
+detailrouter = DefaultRouter()
+detailrouter.register('', UserDetailViewSet)
 #router = routers.DefaultRouter()
 #router.register(r'signup', SignupViewSet)
 app_name = 'accounts'
@@ -17,7 +18,8 @@ app_name = 'accounts'
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', UserListView.as_view(), name='user_list'),
-    path('<int:pk>/', views.user_detail, name='user_detail'),  
+    path('<int:pk>/', include(detailrouter.urls), name='detail')
+    #path('<int:pk>/', views.user_detail, name='user_detail'),  
     path('signup/', SignupView.as_view(), name='signup'),
     path('<int:pk>/update/', UserUpdateView.as_view(), name='update'),
     path('login/', LoginView.as_view(), name='login'),

8ad37cd1851efae1abca7e54fdbcdc1020e0e1a1
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-10-06 10:30:01 +0900

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,15 +3,14 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.routers import DefaultRouter
 from . import views
 
-from accounts.views import UserListView, SignupView, UserActivate, LoginView, PointViewSet, UserPointView, UserUpdateView, UserDetailViewSet
+from accounts.views import UserListView, SignupView, UserActivate, LoginView, PointViewSet, UserPointView, UserDetailViewSet
 
 #라우팅 설정
 router = DefaultRouter()
 router.register('', PointViewSet)
 detailrouter = DefaultRouter()
 detailrouter.register('', UserDetailViewSet)
-#router = routers.DefaultRouter()
-#router.register(r'signup', SignupViewSet)
+
 app_name = 'accounts'
 
 
@@ -21,7 +20,6 @@ urlpatterns = [
     path('<int:pk>/', include(detailrouter.urls), name='detail'),
     #path('<int:pk>/', views.user_detail, name='user_detail'),  
     path('signup/', SignupView.as_view(), name='signup'),
-    #path('<int:pk>/update/', UserUpdateView.as_view(), name='update'),
     path('login/', LoginView.as_view(), name='login'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('activate/<str:uidb64>/<str:token>/', UserActivate.as_view(), name="activate"),

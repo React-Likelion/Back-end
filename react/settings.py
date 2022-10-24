5e6e9b8095879890af1c5f409a0c02c57081d46c
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-10-24 14:32:57 +0900

    Update settings.py

--- react/settings.py
+++ react/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000', 'https://web-react-1b2nj24l6ymdhdc.gksl1.cloudtype.app')
+CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000', 'http://re-act.s3-website.ap-northeast-2.amazonaws.com/')
 CORS_ALLOW_CREDENTIALS = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
